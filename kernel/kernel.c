98e4e74751722cd8bd943de2db044516e6b0a3b7
HelloWorldZTR <2279429006@qq.com>
2025-07-02 20:50:01 +0800

    Feat: make it less of a junk

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -9,8 +9,17 @@ void kernel_main() {
     setcolor(WHITE, BLACK); // Set foreground and background colors
 
     clear();
-    
+
     puts("Hi! Welcome to MMS-DOS!\n");
+
+    while(1) {
+        printf("A:/>");
+        char input[256];
+        gets(input, 256);
+        printf("You entered: %s", input);
+        
+        putchar('\n');
+    }
 
     asm volatile (
         "cli\n" // Disable interrupts
